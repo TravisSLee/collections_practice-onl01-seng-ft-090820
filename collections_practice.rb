e74c298b6b209232fd6352f119ce2a27f6409770
Travis Lee <travisslee223@gmail.com>
2020-09-12 22:11:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,10 +10,5 @@ end
 
 def sort_array_char_count(array)
   array.sort do |a, b|
-    if a.length == b.length
-    0
-  elsif a.length < b.length
-    -1
-  elsif a.length > b.length
-    1
+   a.length <=> b.length
 end
